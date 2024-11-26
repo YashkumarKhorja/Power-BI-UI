388c39b44f445558f53c572bab8761b6d5dd0630
Khorja <2362858@cognizant.com>
2024-11-25 21:44:01 -0400

    first commit

---
+++ src/main/java/com/cognizant/POWER_BI_UI/App.java
@@ -0,0 +1,13 @@
+package com.cognizant.POWER_BI_UI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
